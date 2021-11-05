6020a3b8980431d4b4d4b13c74ead56c1d10515a
chogyejin <whrpwls96@naver.com>
2021-11-05 19:13:34 +0900

    Jest 설치, package.json scripts 추가, fn.js 작성

---
+++ Jest/fn.test.js
@@ -0,0 +1,16 @@
+// npm test : __test__ 폴더, test로 끝나는 파일 테스트
+const fn = require('./fn');
+
+//expect에 검증할 값, toBe에 기대되는 값
+//tobe : Matcher, 기본 타입 값 비교할 때 사용
+test('1은 1이야', () => {
+  expect(1).toBe(1);
+});
+
+test('2+3은 5이야', () => {
+  expect(fn.add(2, 3)).toBe(5);
+});
+
+test('3+3은 5가 아님', () => {
+  expect(fn.add(3, 3)).not.toBe(5);
+});
