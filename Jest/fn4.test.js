6eb1eda861f25a62b6ccb8bb3a38c30b9b4c59fa
chogyejin <whrpwls96@naver.com>
2021-11-08 16:26:25 +0900

    mock 함수, fn4 주석 제거

--- Jest/fn4.test.js
+++ Jest/fn4.test.js
@@ -1,100 +1,100 @@
 const fn = require('./fn');
 
-// let num = 0;
-
 // // beforeEach : 각 테스트 직전에 실행되는 함수
 // // afterEach : 각 테스트 직후 실행
-// beforeEach(() => {
-//   num = 0;
-// });
+let num = 0;
 
-// test('0 더하기 1은 1이다', () => {
-//   num = fn.add(num, 1);
-//   expect(num).toBe(1);
-// });
+beforeEach(() => {
+  num = 0;
+});
 
-// test('0 더하기 2은 2이다', () => {
-//   num = fn.add(num, 2); //num이 계속 바뀌기 때문에 beforeEach로 초기화 필요
-//   expect(num).toBe(2);
-// });
+test('0 더하기 1은 1이다', () => {
+  num = fn.add(num, 1);
+  expect(num).toBe(1);
+});
 
-// test('0 더하기 3은 3이다', () => {
-//   num = fn.add(num, 3);
-//   expect(num).toBe(3);
-// });
+test('0 더하기 2은 2이다', () => {
+  num = fn.add(num, 2); //num이 계속 바뀌기 때문에 beforeEach로 초기화 필요
+  expect(num).toBe(2);
+});
+
+test('0 더하기 3은 3이다', () => {
+  num = fn.add(num, 3);
+  expect(num).toBe(3);
+});
 
 let user;
 // 작업 전에 DB에서 유저 정보 가져온다
 // 작업 후 DB 커넥트 끊는다
 // 각 테스트 케이스마다 DB connent, disconnect할 필요 없기 때문에 beforeAll, afterAll 이용
-// beforeAll(async () => {
-//   user = await fn.connectUserDb();
-// });
-// afterAll(() => {
-//   return fn.disconnectUserDb();
-// });
+beforeAll(async () => {
+  user = await fn.connectUserDb();
+});
+afterAll(() => {
+  return fn.disconnectUserDb();
+});
 
-// test('이름은 Mike', () => {
-//   expect(user.name).toBe('Mike');
-// });
+test('이름은 Mike', () => {
+  expect(user.name).toBe('Mike');
+});
 
-// test('나이는 30', () => {
-//   expect(user.age).toBe(30);
-// });
+test('나이는 30', () => {
+  expect(user.age).toBe(30);
+});
 
-// test('성별은 남자', () => {
-//   expect(user.gender).toBe('male');
-// });
+test('성별은 남자', () => {
+  expect(user.gender).toBe('male');
+});
 
-// // 유사한 작업 descirbe
-// describe('Car 관련 작업', () => {
-//   let car;
+// 유사한 작업 descirbe
+describe('Car 관련 작업', () => {
+  let car;
 
-//   beforeAll(async () => {
-//     car = await fn.connectCarDb();
-//   });
-//   afterAll(() => {
-//     return fn.disconnectCarDb();
-//   });
+  beforeAll(async () => {
+    car = await fn.connectCarDb();
+  });
+  afterAll(() => {
+    return fn.disconnectCarDb();
+  });
 
-//   test('브랜드는 벤츠', () => {
-//     expect(car.brand).toBe('Benz');
-//   });
+  test('브랜드는 벤츠', () => {
+    expect(car.brand).toBe('Benz');
+  });
 
-//   test('차종은 GLA', () => {
-//     expect(car.name).toBe('GLA');
-//   });
+  test('차종은 GLA', () => {
+    expect(car.name).toBe('GLA');
+  });
 
-//   test('색은 검은색', () => {
-//     expect(car.color).toBe('black');
-//   });
-// });
+  test('색은 검은색', () => {
+    expect(car.color).toBe('black');
+  });
+});
 
 // 실행 순서
-//밖 beforeEach, after는 describe 안 테스트에도 영향
-//밖 beforeEach는 describe 안 beforeEach보다 먼저
-//밖 afterEach는 describe 안 afterEach보다 나중
-// beforeAll(() => console.log('밖 beforeAll')); // 1
-// beforeEach(() => console.log('밖 beforeEach')); //2, 6
-// afterEach(() => console.log('밖 afterEach')); //4, 10
-// afterAll(() => console.log('밖 afterAll')); // 마지막
+// 밖 beforeEach, after는 describe 안 테스트에도 영향
+// 밖 beforeEach는 describe 안 beforeEach보다 먼저
+// 밖 afterEach는 describe 안 afterEach보다 나중
+beforeAll(() => console.log('밖 beforeAll')); // 1
+beforeEach(() => console.log('밖 beforeEach')); //2, 6
+afterEach(() => console.log('밖 afterEach')); //4, 10
+afterAll(() => console.log('밖 afterAll')); // 마지막
 
-// test('0 + 1 = 1', () => {
-//   console.log('밖 test');
-//   expect(fn.add(0, 1)).toBe(1); //3
-// });
+test('0 + 1 = 1', () => {
+  console.log('밖 test');
+  expect(fn.add(0, 1)).toBe(1); //3
+});
 
-// describe('describe 작업', () => {
-//   beforeAll(() => console.log('안 beforeAll')); //5
-//   beforeEach(() => console.log('안 beforeEach')); //7
-//   afterEach(() => console.log('안 afterEach')); //9
-//   afterAll(() => console.log('안 afterAll')); //마지막에서 두 번째
+describe('describe 작업', () => {
+  beforeAll(() => console.log('안 beforeAll')); //5
+  beforeEach(() => console.log('안 beforeEach')); //7
+  afterEach(() => console.log('안 afterEach')); //9
+  afterAll(() => console.log('안 afterAll')); //마지막에서 두 번째
 
-//   test('0 + 1 = 1', () => {
-//     console.log('안 test');
-//     expect(fn.add(0, 1)).toBe(1); //8
-//   });
-// });
+  test('0 + 1 = 1', () => {
+    console.log('안 test');
+    expect(fn.add(0, 1)).toBe(1); //8
+  });
+});
 
 // test 케이스 내, 외부 원인 찾기
 // test.only로 다른 케이스 스킵
---
+++ Jest/fn5.test.js
@@ -0,0 +1,83 @@
+// mock function
+
+// const mockFn = jest.fn();
+
+// mockFn();
+// mockFn(1);
+
+// // mockFn 함수에는 mock이라는 property와 calls라는 배열이 있음
+// // 함수가 총 몇 번 호출되었는지, 전달된 인수는 뭔지 파악 가능
+// test('함수는 2번 호출된다', () => {
+//   expect(mockFn.mock.calls.length).toBe(2);
+// });
+
+// test('2번째로 호출된 함수의 첫 번째 인수는 1이다', () => {
+//   expect(mockFn.mock.calls[1][0]).toBe(1);
+// });
+
+// const mockFn2 = jest.fn();
+// function forEachAdd1(arr) {
+//   arr.forEach((num) => {
+//     mockFn2(num + 1);
+//   });
+// }
+
+// forEachAdd1([10, 20, 30]);
+
+// test('함수는 3번 호출된다', () => {
+//   expect(mockFn2.mock.calls.length).toBe(3);
+// });
+
+// test('전달된 값은 1씩 증가한 11, 21, 31이다', () => {
+//   expect(mockFn2.mock.calls[0][0]).toBe(11);
+//   expect(mockFn2.mock.calls[1][0]).toBe(21);
+//   expect(mockFn2.mock.calls[2][0]).toBe(31);
+// });
+
+// 어떤 값을 리턴하는 함수가 필요할 때
+// const mockFn3 = jest.fn((num) => num + 1);
+
+// mockFn3(10);
+// mockFn3(20);
+// mockFn3(30);
+
+// // mock.results는 type과 value를 가진 객체 배열
+// // [
+// //   { type: 'return', value: 11 },
+// //   { type: 'return', value: 21 },
+// //   { type: 'return', value: 31 },
+// // ];
+// // test('함수는 3번 호출된다', () => {
+// //   console.log(mockFn3.mock.results);
+// //   expect(mockFn3.mock.calls.length).toBe(3);
+// // });
+
+// test('10에서 1 증가는 11', () => {
+//   expect(mockFn3.mock.results[0].value).toBe(11);
+// });
+
+// test('20에서 1 증가는 21', () => {
+//   expect(mockFn3.mock.results[1].value).toBe(21);
+// });
+
+// test('30에서 1 증가는 31', () => {
+//   expect(mockFn3.mock.results[2].value).toBe(31);
+// });
+
+// mockFn.mockReturnValueOnce, mockFn.mockReturnValue : 중간에 리턴값 변경
+// callback 함수 대신 먼저 mock 함수 이용
+// 홀수만 찾기
+const mockFn4 = jest.fn();
+
+mockFn4
+  .mockReturnValueOnce(true)
+  .mockReturnValueOnce(false)
+  .mockReturnValueOnce(true)
+  .mockReturnValueOnce(false)
+  .mockReturnValue(true);
+
+const result = [1, 2, 3, 4, 5].filter((num) => mockFn4(num));
+
+test('홀수는 1, 3, 5', () => {
+  expect(result).toStrictEqual([1, 3, 5]);
+});
